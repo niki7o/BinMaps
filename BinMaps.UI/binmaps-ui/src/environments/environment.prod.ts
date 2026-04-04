33dffc013863bac438341c43acac6d394f53a5cd
niki7o <nikibg4334@gmail.com>
2026-04-04 22:29:07 +0300

    env

--- BinMaps.UI/binmaps-ui/src/environments/environment.prod.ts
+++ BinMaps.UI/binmaps-ui/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: '/api',
   hubUrl: '/hubs/containers',
-  mapboxToken: 'pk.eyJ1IjoibmlrMXQwIiwiYSI6ImNtbmo5dTI1MDBka2oycHI2ajVwdGx2em4ifQ.p0vu1nTUkTthyP7uc7dAcA'
+  mapboxToken: '${MAPBOX_TOKEN}'
 };
