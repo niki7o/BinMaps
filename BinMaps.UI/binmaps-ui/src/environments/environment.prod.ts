2911f1e333835d5404946b6cce88399abd207de1
niki7o <nikibg4334@gmail.com>
2026-04-04 22:22:47 +0300

    fixed git

---
+++ BinMaps.UI/binmaps-ui/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: '/api',
+  hubUrl: '/hubs/containers',
+  mapboxToken: 'pk.eyJ1IjoibmlrMXQwIiwiYSI6ImNtbmo5dTI1MDBka2oycHI2ajVwdGx2em4ifQ.p0vu1nTUkTthyP7uc7dAcA'
+};
